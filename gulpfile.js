3175c518460e40b17b787ae922ac40416bc6919c
Pavel Ivashkov <pavels.code@gmail.com>
2017-08-29 23:30:55 +0300

    + dist webstore and mozilla

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ const platform = (process.env.PLATFORM || 'chrome').toLowerCase()
 
 
 const build = {
-    root: 'dist/build/' + platform
+    root: 'dist/build',
+    platform: 'dist/build/' + platform
 }
 
 
@@ -28,11 +29,13 @@ const paths = {
     },
 
     dest: {
-        js: build.root + '/js',
-        html: build.root,
-        img: build.root + '/img',
-        translations: build.root + '/_locales',
-        manifest: build.root,
+        js: build.platform + '/js',
+        html: build.platform,
+        img: build.platform + '/img',
+        translations: build.platform + '/_locales',
+        manifest: build.platform,
+        all_files: build.platform + '/**/*',
+        dist_archive: build.root + '/' + platform + '.zip',
     }
 }
 
@@ -44,60 +47,68 @@ const gulp = require('gulp')
     , json5 = require('gulp-json5-to-json')
     , mergejson = require('gulp-merge-json')
     , concat = require('gulp-concat')
+    , zip = require('gulp-vinyl-zip')
 
 
-gulp.task('scripts:content', () => {
+gulp.task('scripts:content', (cb) => {
     gulp.src([
         paths.src.jsroot + '/settings.js',
         paths.src.jsroot + '/content.js',
         ])
         .pipe(concat('content.js'))
         .pipe(gulp.dest(paths.dest.js))
+        .on('end', cb)
 })
 
 
-gulp.task('scripts:popup', () => {
+gulp.task('scripts:popup', (cb) => {
     gulp.src([
         paths.src.jsroot + '/settings.js',
         paths.src.jsroot + '/popup.js',
         ])
         .pipe(concat('popup.js'))
         .pipe(gulp.dest(paths.dest.js))
+        .on('end', cb)
 })
 
 
-gulp.task('scripts:background', () => {
+gulp.task('scripts:background', (cb) => {
     gulp.src([
         paths.src.jsroot + '/settings.js',
         paths.src.jsroot + '/background.js',
         ])
         .pipe(concat('background.js'))
         .pipe(gulp.dest(paths.dest.js))
+        .on('end', cb)
 })
 
 
-gulp.task('scripts', ['scripts:content', 'scripts:popup', 'scripts:background'])
+gulp.task('scripts', ['scripts:content', 'scripts:popup', 'scripts:background'], () => {
+})
 
 
-gulp.task('pages', () => {
+gulp.task('pages', (cb) => {
     gulp.src(paths.src.html)
         .pipe(gulp.dest(paths.dest.html))
+        .on('end', cb)
 })
 
 
-gulp.task('images', () => {
+gulp.task('images', (cb) => {
     gulp.src(paths.src.img)
         .pipe(gulp.dest(paths.dest.img))
+        .on('end', cb)
 })
 
 
-gulp.task('data', () => {
+gulp.task('data', (cb) => {
     gulp.src(paths.src.translations)
         .pipe(gulp.dest(paths.dest.translations))
+        .on('end', cb)
 })
 
 
-gulp.task('manifest', () => {
+gulp.task('manifest', (cb) => {
     gulp.src([paths.src.manifest, paths.platform.manifest])
         .pipe(mergejson({
             json5: true,
@@ -105,14 +116,16 @@ gulp.task('manifest', () => {
         }))
         .pipe(json5())
         .pipe(gulp.dest(paths.dest.manifest))
+        .on('end', cb)
 })
 
 
-gulp.task('build', ['scripts', 'pages', 'images', 'data', 'manifest'])
+gulp.task('build', ['scripts', 'pages', 'images', 'data', 'manifest'], () => {
+})
 
 
 gulp.task('clean', () => {
-    del(build.root)
+    del(build.platform)
 })
 
 
@@ -125,4 +138,10 @@ gulp.task('watch', () => {
 })
 
 
-gulp.task('default', ['clean', 'build', 'watch'])
+gulp.task('dist', ['clean', 'build'], () => {
+    gulp.src(paths.dest.all_files)
+        .pipe(zip.dest(paths.dest.dist_archive))
+})
+
+
+gulp.task('default', ['clean', 'build'], () => {})
