b346fa364687910cf6f6cc4eae45751d36bfc3e3
Pavel Ivashkov <pavels.code@gmail.com>
2017-09-01 23:22:09 +0300

    :1234: show version in popup

--- src/js/html_i18n.js
+++ src/js/html_i18n.js
@@ -1,11 +1,11 @@
 
-const i18n = window.i18n || (window.chrome && window.chrome.i18n)
+const browserapi = chrome
 
 
 class HtmlLocalizer {
     constructor() {
         this.includeMatching = /__MSG_/
-        this.extractKey = /__MSG_(\w+?)__/
+        this.extractKey = /__MSG_(\w+?)__/g
         this.replaceMatching = /__MSG_\w+?__/
     }
 
@@ -16,17 +16,30 @@ class HtmlLocalizer {
 
         let child;
         while (child = it.nextNode()) {
-            const matches = this.extractKey.exec(child.data)
-            if (matches) {
+            let textContent = child.data || ''
+            let matches = undefined
+
+            while (matches = this.extractKey.exec(child.data)) {
                 const key = matches[1]
-                const value = i18n.getMessage(key)
-                if (value) {
-                    child.data = child.data.replace(this.replaceMatching, value)
+
+                if (key === 'manifest_json_version') {
+                    const manifest = browserapi.runtime.getManifest()
+                    const value = manifest.version
+                    textContent = textContent.replace(this.replaceMatching, value)
                 }
                 else {
-                    console.log('missing localization for ', key)
+                    const value = browserapi.i18n.getMessage(key)
+                    if (value) {
+                        textContent = textContent.replace(this.replaceMatching, value)
+                    }
+                    else {
+                        console.log('missing localization for ', key)
+                    }
                 }
+
             }
+
+            child.data = textContent
         }
     }
 }
