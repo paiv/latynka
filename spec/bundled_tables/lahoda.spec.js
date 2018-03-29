fd6f2f2ad5b8ce59b6f70654b40fd3b9cb6848a1
Pavel Ivashkov <pavels.code@gmail.com>
2018-03-29 19:30:13 +0300

    :ukraine: lahoda

--- spec/bundled_tables/lahoda.spec.js
+++ spec/bundled_tables/lahoda.spec.js
@@ -21,7 +21,7 @@ describe('Lahoda 2008', function() {
 
     it('converts шщ chars', function() {
         const converted = this.convert('ш Ш щ')
-        expect(converted).toBe('s\u030C Š šč')
+        expect(converted).toBe('š Š šč')
     })
 
     it('converts иіїй chars', function() {
@@ -43,7 +43,7 @@ describe('Lahoda 2008', function() {
 
         it('converts жз chars', function() {
             const converted = this.convert('ж Ж з')
-            expect(converted).toBe('z\u030C Ž z')
+            expect(converted).toBe('ž Ž z')
         })
 
         it('converts л', function() {
@@ -73,7 +73,7 @@ describe('Lahoda 2008', function() {
 
         it('converts цч chars', function() {
             const converted = this.convert('ц Ц ч Ч')
-            expect(converted).toBe('c C c\u030C Č')
+            expect(converted).toBe('c C č Č')
         })
     })
 
@@ -81,42 +81,42 @@ describe('Lahoda 2008', function() {
 
         it('converts дь char', function() {
             const converted = this.convert('дь Дь дє дю дя дьє дьйо дью дья')
-            expect(converted).toBe('d\u030C Ď ďe ďu ďa ďie ďio ďiu ďia')
+            expect(converted).toBe('ď Ď ďe ďu ďa ďie ďio ďiu ďia')
         })
 
         it('converts зь', function() {
             const converted = this.convert('зь Зь зє зю зя зьє зьйо зью зья')
-            expect(converted).toBe('z\u0301 Ź źe źu źa źie źio źiu źia')
+            expect(converted).toBe('ź Ź źe źu źa źie źio źiu źia')
         })
 
         it('converts ль', function() {
             const converted = this.convert('ль ЛЬ лє лю ля льє льйо лью лья')
-            expect(converted).toBe('l\u030C Ľ ľe ľu ľa ľie ľio ľiu ľia')
+            expect(converted).toBe('ľ Ľ ľe ľu ľa ľie ľio ľiu ľia')
         })
 
         it('converts нь', function() {
             const converted = this.convert('нь Нь нє ню ня ньє ньйо нью нья')
-            expect(converted).toBe('n\u0301 Ń ńe ńu ńa ńie ńio ńiu ńia')
+            expect(converted).toBe('ń Ń ńe ńu ńa ńie ńio ńiu ńia')
         })
 
         it('converts рь char', function() {
             const converted = this.convert('рь Рь рє рю ря')
-            expect(converted).toBe('r\u0301 Ŕ ŕe ŕu ŕa')
+            expect(converted).toBe('ŕ Ŕ ŕe ŕu ŕa')
         })
 
         it('converts сь char', function() {
             const converted = this.convert('сь Сь сє сю ся сьє сьйо сью сья')
-            expect(converted).toBe('s\u0301 Ś śe śu śa śie śio śiu śia')
+            expect(converted).toBe('ś Ś śe śu śa śie śio śiu śia')
         })
 
         it('converts ть char', function() {
             const converted = this.convert('ть Ть тє тю тя тьє тьйо тью тья')
-            expect(converted).toBe('t\u030C Ť ťe ťu ťa ťie ťio ťiu ťia')
+            expect(converted).toBe('ť Ť ťe ťu ťa ťie ťio ťiu ťia')
         })
 
         it('converts ць chars', function() {
             const converted = this.convert('ць Ць цє цю ця цьє цьйо цью цья')
-            expect(converted).toBe('c\u0301 Ć će ću ća ćie ćio ćiu ćia')
+            expect(converted).toBe('ć Ć će ću ća ćie ćio ćiu ćia')
         })
     })
 
@@ -124,7 +124,7 @@ describe('Lahoda 2008', function() {
 
         it('converts consonant-єюя', function() {
             const converted = this.convert('тє тю тя')
-            expect(converted).toBe('ťe ťu ťa')
+            expect(converted).toBe('ťe ťu ťa')
         })
 
         it('converts word start єюя', function() {
@@ -147,12 +147,12 @@ describe('Lahoda 2008', function() {
 
         it('converts іа іе іу', function() {
             const converted = this.convert("іа іе іу")
-            expect(converted).toBe('ía íe íu')
+            expect(converted).toBe('ía íe íu')
         })
 
         it('converts ий', function() {
             const converted = this.convert("ий")
-            expect(converted).toBe('y\u0301')
+            expect(converted).toBe('ý')
         })
 
         it('converts вв', function() {
@@ -173,12 +173,12 @@ describe('Lahoda 2008', function() {
 
     it('converts pangram', function() {
         const converted = this.convert('Щастям б\'єш жук їх глицю в фон й ґедзь пріч.')
-        expect(converted).toBe('Ščasťam bješ žuk jich hlyću v fon j gedź prič.')
+        expect(converted).toBe('Ščasťam bješ žuk jich hlyću v fon j gedź prič.')
     })
 
     it('converts official testset', function() {
         const tests = [
-            ['авіабаза', 'avíabaza'],
+            ['авіабаза', 'avíabaza'],
             ['ангел', 'anhel'],
             ['абсурд', 'absurd'],
             ['акцент', 'akcent'],
@@ -188,32 +188,31 @@ describe('Lahoda 2008', function() {
             ['цукор', 'cukor'],
             ['церемонія', 'ceremonija'],
             ['цивілізація', 'cyvilizacija'],
-            ['ця', 'ća'],
-            ['одиниця', 'odynyća'],
-            ['чай', 'čaj'],
-            ['черешня', 'čerešńa'],
-            ['добрий день', 'dobrý deń'],
-            ['дякую', 'ďakuju'],
+            ['ця', 'ća'],
+            ['одиниця', 'odynyća'],
+            ['чай', 'čaj'],
+            ['черешня', 'čerešńa'],
+            ['добрий день', 'dobrý deń'],
+            ['дякую', 'ďakuju'],
             ['Европа', 'Evropa'],
-            ['елегантний', 'elehantný'],
-            ['Запоріжжя', 'Zaporižžia'],
+            ['елегантний', 'elehantný'],
+            ['Запоріжжя', 'Zaporižžia'],
             ['зв’язок', 'zvjazok'],
             ['з’їсти', 'zjisty'],
             ['Франція', 'Francija'],
             ['Іван Франко', 'Ivan Franko'],
             ['ґрунт', 'grunt'],
-            ['ґуля', 'guľa'], // orig: ґула
+            ['ґуля', 'guľa'], // orig: ґула
             ['організація', 'orhanizacija'],
             ['гумор', 'humor'],
             ['гардероб', 'harderob'],
             ['характер', 'charakter'],
             ['монархія', 'monarchija'],
             ['ідеал', 'ideal'],
-            ['Ізраїль', 'Izrajiľ'],
+            ['Ізраїль', 'Izrajiľ'],
             ['ідол', 'idol'],
             ['ім’я', 'imja'],
             ['свята', 'sviata'],
-            ['авіабаза', 'avíabaza'],
             ['Йорданія', 'Jordanija'],
             ['Юдаїзм', 'Judajizm'],
             ['кабінет', 'kabinet'],
@@ -224,57 +223,57 @@ describe('Lahoda 2008', function() {
             ['кур’єр', 'kurier'],
             ['Лондон', 'London'],
             ['ліфт', 'lift'],
-            ['людина', 'ľudyna'],
-            ['льодовий', 'ľodový'],
-            ['любов’ю', 'ľubovju'],
-            ['медаль', 'medaľ'],
+            ['людина', 'ľudyna'],
+            ['льодовий', 'ľodový'],
+            ['любов’ю', 'ľubovju'],
+            ['медаль', 'medaľ'],
             ['механізм', 'mechanizm'],
-            ['мільйон', 'miľion'],
-            ['народний', 'narodný'],
+            ['мільйон', 'miľion'],
+            ['народний', 'narodný'],
             ['нація', 'nacija'],
-            ['національний', 'nacionaľný'],
-            ['намагання', 'namahanńa'],
+            ['національний', 'nacionaľný'],
+            ['намагання', 'namahanńa'],
             ['Ніобій', 'Niobij'],
-            ['нотаріус', 'notaríus'],
-            ['рельєф', 'reľief'],
-            ['рівень', 'riveń'],
-            ['обличчя', 'oblyččia'],
+            ['нотаріус', 'notaríus'],
+            ['рельєф', 'reľief'],
+            ['рівень', 'riveń'],
+            ['обличчя', 'oblyččia'],
             ['обов’язок', 'obovjazok'],
-            ['обов’язковий', 'obovjazkový'],
+            ['обов’язковий', 'obovjazkový'],
             ['опозиція', 'opozycija'],
-            ['пікантний', 'pikantný'],
+            ['пікантний', 'pikantný'],
             ['премія', 'premija'],
             ['оратор', 'orator'],
             ['рація', 'racija'],
-            ['темрява', 'temŕava'],
-            ['уряд', 'uŕad'],
-            ['рятувати', 'ŕatuvaty'],
-            ['сальдо', 'saľdo'],
+            ['темрява', 'temŕava'],
+            ['уряд', 'uŕad'],
+            ['рятувати', 'ŕatuvaty'],
+            ['сальдо', 'saľdo'],
             ['сода', 'soda'],
-            ['службовий', 'službový'],
-            ['сьогодні', 'śohodni'],
-            ['серйозний', 'seriozný'],
-            ['народжуються', 'narodžujuťśa'],
-            ['шампунь', 'šampuń'],
-            ['Швеція', 'Švecija'],
+            ['службовий', 'službový'],
+            ['сьогодні', 'śohodni'],
+            ['серйозний', 'seriozný'],
+            ['народжуються', 'narodžujuťśa'],
+            ['шампунь', 'šampuń'],
+            ['Швеція', 'Švecija'],
             ['табу', 'tabu'],
             ['телеграма', 'telehrama'],
-            ['телебачення', 'telebačenńa'],
-            ['тяжко', 'ťažko'],
-            ['тюльпан', 'ťuľpan'],
-            ['Умань', 'Umań'],
-            ['уважний', 'uvažný'],
+            ['телебачення', 'telebačenńa'],
+            ['тяжко', 'ťažko'],
+            ['тюльпан', 'ťuľpan'],
+            ['Умань', 'Umań'],
+            ['уважний', 'uvažný'],
             ['Ватикан', 'Vatykan'],
-            ['вільно', 'viľno'],
+            ['вільно', 'viľno'],
             ['введи', 'wedy'],
-            ['вважати', 'wažaty'],
+            ['вважати', 'wažaty'],
             ['Ватикан', 'Vatykan'],
             ['індик', 'indyk'],
-            ['національний', 'nacionaľný'],
-            ['зелений', 'zelený'],
-            ['зять', 'źať'],
-            ['жовтень', 'žovteń'],
-            ['жнива', 'žnyva'],
+            ['національний', 'nacionaľný'],
+            ['зелений', 'zelený'],
+            ['зять', 'źať'],
+            ['жовтень', 'žovteń'],
+            ['жнива', 'žnyva'],
         ]
 
         tests.forEach((t) => {
