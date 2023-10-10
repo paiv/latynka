931e3cc29e104827fed772a64ec38ff428369c65
Pavlo Ivashkov <pavels.code@gmail.com>
2023-10-10 23:17:57 +0300

    🇺🇦 avtorska

--- spec/bundled_tables/paiv.spec.js
+++ spec/bundled_tables/paiv.spec.js
@@ -16,12 +16,12 @@ describe('PAIV', function() {
 
     it('converts гґх chars', function() {
         const converted = this.convert('г Г ґ Ґ х Х')
-        expect(converted).toBe('g G ǧ Ǧ x X')
+        expect(converted).toBe('ğ Ğ g G x X')
     })
 
     it('converts шщ chars', function() {
-        const converted = this.convert('ш Ш щ Щ')
-        expect(converted).toBe('š Š šč Šč')
+        const converted = this.convert('ш Ш щ Щ шч')
+        expect(converted).toBe('š Š šč Šč š\'č')
     })
 
     it('converts иіїй chars', function() {
@@ -74,34 +74,64 @@ describe('PAIV', function() {
 
     describe('soft', function() {
 
+        it('converts гь char', function() {
+            const converted = this.convert('гь Гь гє гю гя')
+            expect(converted).toBe('ǵ Ǵ ǵe ǵu ǵa')
+        })
+
+        it('converts ґь char', function() {
+            const converted = this.convert('ґь Ґь ґє ґю ґя')
+            expect(converted).toBe('ǵ Ǵ ǵe ǵu ǵa')
+        })
+
         it('converts дь char', function() {
             const converted = this.convert('дь Дь дє дю дя')
             expect(converted).toBe('d\u0301 D\u0301 d\u0301e d\u0301u d\u0301a')
         })
 
+        it('converts жь char', function() {
+            const converted = this.convert('жь Жь жє жю жя')
+            expect(converted).toBe('ž Ž že žu ža')
+        })
+
         it('converts зь char', function() {
             const converted = this.convert('зь Зь зє зю зя')
-            expect(converted).toBe('z\u0301 Z\u0301 z\u0301e z\u0301u z\u0301a')
+            expect(converted).toBe('ź Ź źe źu źa')
+        })
+
+        it('converts кь char', function() {
+            const converted = this.convert('кь Кь кє кю кя')
+            expect(converted).toBe('ḱ Ḱ ḱe ḱu ḱa')
         })
 
         it('converts ль char', function() {
             const converted = this.convert('ль ЛЬ лє лю ля')
-            expect(converted).toBe('l\u0301 L\u0301 l\u0301e l\u0301u l\u0301a')
+            expect(converted).toBe('ĺ Ĺ ĺe ĺu ĺa')
+        })
+
+        it('converts мь char', function() {
+            const converted = this.convert('мь Мь мє мю мя')
+            expect(converted).toBe('ḿ Ḿ ḿe ḿu ḿa')
         })
 
         it('converts нь char', function() {
             const converted = this.convert('нь Нь нє ню ня')
-            expect(converted).toBe('n\u0301 N\u0301 n\u0301e n\u0301u n\u0301a')
+            expect(converted).toBe('ń Ń ńe ńu ńa')
+        })
+
+        it('converts пь char', function() {
+            const converted = this.convert('пь Пь пє пю пя')
+            expect(converted).toBe('ṕ Ṕ ṕe ṕu ṕa')
         })
 
         it('converts рь char', function() {
             const converted = this.convert('рь Рь рє рю ря')
-            expect(converted).toBe('r\u0301 R\u0301 r\u0301e r\u0301u r\u0301a')
+            expect(converted).toBe('ŕ Ŕ ŕe ŕu ŕa')
         })
 
         it('converts сь char', function() {
             const converted = this.convert('сь Сь сє сю ся')
-            expect(converted).toBe('s\u0301 S\u0301 s\u0301e s\u0301u s\u0301a')
+            expect(converted).toBe('ś Ś śe śu śa')
         })
 
         it('converts ть char', function() {
@@ -109,9 +139,34 @@ describe('PAIV', function() {
             expect(converted).toBe('t\u0301 T\u0301 t\u0301e t\u0301u t\u0301a')
         })
 
+        it('converts ться ending', function() {
+            const converted = this.convert('ться')
+            expect(converted).toBe('tśa')
+        })
+
         it('converts ць char', function() {
             const converted = this.convert('ць Ць цє цю ця')
-            expect(converted).toBe('c\u0301 C\u0301 c\u0301e c\u0301u c\u0301a')
+            expect(converted).toBe('ć Ć će ću ća')
+        })
+
+        it('converts чь char', function() {
+            const converted = this.convert('чь Чь чє чю чя')
+            expect(converted).toBe('č Č če ču ča')
+        })
+        
+        it('converts шчь combo', function() {
+            const converted = this.convert('шчь шчє шчю шчя')
+            expect(converted).toBe("š'č š'če š'ču š'ča")
+        })
+
+        it('converts шь char', function() {
+            const converted = this.convert('шь Шь шє шю шя')
+            expect(converted).toBe('š Š še šu ša')
+        })
+
+        it('converts щь char', function() {
+            const converted = this.convert('щь Щь щє щю щя')
+            expect(converted).toBe('šč Šč šče šču šča')
         })
     })
 
@@ -137,9 +192,9 @@ describe('PAIV', function() {
             expect(converted).toBe('je ju ja ji')
         })
 
-        it('converts ьї', function() {
-            const converted = this.convert("тьї")
-            expect(converted).toBe('t\u0301ji')
+        it('converts ь-аеуї', function() {
+            const converted = this.convert("тьа тье тьу тьї")
+            expect(converted).toBe("t\u0301'a t\u0301'e t\u0301'u t\u0301ji")
         })
 
         describe('borrowed', function() {
@@ -148,11 +203,16 @@ describe('PAIV', function() {
                 const converted = this.convert('йє йю йя йї')
                 expect(converted).toBe('jje jju jja jji')
             })
+
+            it('converts й-аеуі', function() {
+                const converted = this.convert('йа йе йу йі')
+                expect(converted).toBe("j'a j'e j'u j'i")
+            })
         })
     })
 
     it('converts pangram', function() {
         const converted = this.convert('Щастям б\'єш жук їх глицю в фон й ґедзь пріч.')
-        expect(converted).toBe('Ščast́am bješ žuk ïx glyću v fon j ǧedź prič.')
+        expect(converted).toBe('Ščast́am bješ žuk ïx ğlyću v fon j gedź prič.')
     })
 })
