d639b269fafe00d1a39bd489474f0f1ca551ace4
Pavlo Ivashkov <pavels.code@gmail.com>
2024-07-15 00:11:30 +0300

    resolve build issues

--- platform/safari/app/ci_scripts/ci_post_clone.sh
+++ platform/safari/app/ci_scripts/ci_post_clone.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 brew install 'node@18'
+export PATH="/usr/local/opt/node@18/bin:$PATH"
 npm install --prefix "$CI_PRIMARY_REPOSITORY_PATH"
--- platform/safari/app/ci_scripts/ci_pre_xcodebuild.sh
+++ platform/safari/app/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
+export PATH="/usr/local/opt/node@18/bin:$PATH"
 npm run --prefix "$CI_PRIMARY_REPOSITORY_PATH" 'build:safari'
 (cd "$CI_PROJECT_FILE_PATH"/../../ && ./build.sh)
